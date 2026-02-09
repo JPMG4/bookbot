80a0daad98a9436957e066db43a06e16e92b8f20
JPGM4 <jmendezpablo558@gmail.com>
2026-02-09 17:04:56 -0500

    Updating logic main.py

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
-import sys
-from stats import count_words, count_characters, chars_dict_to_sorted_list
-
 def main():
-    args = sys.argv
-    if len(args) != 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-    book_path = args[1]
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
 
-    with open(book_path) as f:
-        text = f.read()
-
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    num_words = count_words(text)
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-
-    chars_dict = count_characters(text)
-    sorted_chars = chars_dict_to_sorted_list(chars_dict)
-
-    for item in sorted_chars:
-        ch = item["char"]
-        if not ch.isalpha():
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
             continue
-        print(f"{ch}: {item['num']}")
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
-    print("============= END ===============")
+    print("--- End report ---")
 
-if __name__ == "__main__":
-    main()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
